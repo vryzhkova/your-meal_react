1171caac257fa8d1f5eff71180bbe1c72a083303
Viktoryia Ryzhkova <viktiria87654321@gmail.com>
2023-03-06 16:21:31 +0400

    get goods from server

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -1,29 +1,33 @@
+import { useDispatch, useSelector } from "react-redux";
 import { CatalogProduct } from "../CatalogProduct/CatalogProduct.jsx";
 import { Container } from "../Container/Container.jsx";
 import { Order } from "../Order/Order.jsx";
 import style from "./Catalog.module.css";
-
-const goodsList = [
-  { title: "Мясная бомба" },
-  { title: "Супер сырный" },
-  { title: "Сытный" },
-  { title: "Итальянский" },
-  { title: "Вечная классика" },
-  { title: "Тяжелый удар" },
-];
+import { useEffect } from "react";
+import { productRequestAsync } from "../../store/product/productSlice.js";
 
 export const Catalog = () => {
+  const { products } = useSelector((state) => state.product);
+  const dispatch = useDispatch();
+  const { category, activeCategory } = useSelector((state) => state.category);
+
+  useEffect(() => {
+    if (category.length) {
+      dispatch(productRequestAsync(category[activeCategory].title));
+    }
+  }, [category, activeCategory]);
+
   return (
     <section className={style.catalog}>
       <Container className={style.container}>
         <Order />
         <div className={style.wrapper}>
-          <h2 className={style.title}>Бургеры</h2>
+          <h2 className={style.title}>{category[activeCategory]?.rus}</h2>
           <div className={style.wrap_list}>
             <ul className={style.list}>
-              {goodsList.map((item, i) => (
-                <li key={i} className={style.item}>
-                  <CatalogProduct title={item.title} />
+              {products.map((item) => (
+                <li key={item.i} className={style.item}>
+                  <CatalogProduct item={item} />
                 </li>
               ))}
             </ul>
--- src/components/CatalogProduct/CatalogProduct.jsx
+++ src/components/CatalogProduct/CatalogProduct.jsx
@@ -1,19 +1,25 @@
+import { API_URI } from "../../const";
 import style from "./CatalogProduct.module.css";
 
-export const CatalogProduct = (props) => {
+export const CatalogProduct = ({ item }) => {
   return (
     <article className={style.product}>
-      <img src="img/photo-1.jpg" alt={props.title} className={style.image} />
+      <img
+        src={`${API_URI}/${item.image}`}
+        alt={item.title}
+        className={style.image}
+      />
 
       <p className={style.price}>
-        550<span className="currency">₽</span>
+        {item.price}
+        <span className="currency">₽</span>
       </p>
 
       <h3 className={style.title}>
-        <button className={style.detail}>{props.title}</button>
+        <button className={style.detail}>{item.title}</button>
       </h3>
 
-      <p className={style.weight}>512г</p>
+      <p className={style.weight}>{item.weight}</p>
 
       <button className={style.add} type="button">
         Добавить
