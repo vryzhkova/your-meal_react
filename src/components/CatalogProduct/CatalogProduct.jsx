6529619057101bf1d0000fc1ec79e52cfef0e793
Viktoryia Ryzhkova <viktiria87654321@gmail.com>
2023-03-07 16:19:41 +0400

    added goods to card

--- src/components/CatalogProduct/CatalogProduct.jsx
+++ src/components/CatalogProduct/CatalogProduct.jsx
@@ -1,7 +1,11 @@
+import { useDispatch } from "react-redux";
 import { API_URI } from "../../const";
 import style from "./CatalogProduct.module.css";
+import { addProduct } from "../../store/order/orderSlice";
 
 export const CatalogProduct = ({ item }) => {
+  const dispatch = useDispatch();
+
   return (
     <article className={style.product}>
       <img
@@ -21,7 +25,13 @@ export const CatalogProduct = ({ item }) => {
 
       <p className={style.weight}>{item.weight}</p>
 
-      <button className={style.add} type="button">
+      <button
+        className={style.add}
+        type="button"
+        onClick={() => {
+          dispatch(addProduct({ id: item.id }));
+        }}
+      >
         Добавить
       </button>
     </article>
